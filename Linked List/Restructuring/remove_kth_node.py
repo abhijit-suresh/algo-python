9dfa744042c6cae11f9ef420cea120db3eb1ef50
Abhijit Suresh <abhijit@Abhijits-MacBook-Pro.local>
2025-12-16 20:09:53 -0800

    Linked List: Restructuring

---
+++ Linked List/Restructuring/remove_kth_node.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        dummy.next = head
+        trailer = leader = dummy
+
+        for _ in range(n):
+            leader = leader.next
+            if not leader:
+                return dummy.next
+
+        while leader.next:
+            leader = leader.next
+            trailer = trailer.next
+
+        trailer.next = trailer.next.next
+        return dummy.next
